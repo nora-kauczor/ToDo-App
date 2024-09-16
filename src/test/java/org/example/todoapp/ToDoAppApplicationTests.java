8cc592e35a87b7b37169027efbfe8026313f7b93
Nora Kauczor <n.kauczor@web.de>
2024-09-16 14:44:50 +0200

    add flapdoodle dependency

--- src/test/java/org/example/todoapp/ToDoAppApplicationTests.java
+++ src/test/java/org/example/todoapp/ToDoAppApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ToDoAppApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
